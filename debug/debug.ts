7140e548dba7528e8d7ba1f9877257aeeac3dad3
i-play-lf2 <914637118@qq.com>
2021-03-02 16:59:54 +0800

    update debug

--- debug/debug.ts
+++ debug/debug.ts
@@ -1,6 +1,32 @@
-import { ns, nsr } from "no-stream";
+import { ans } from "no-stream";
 
-ns([1, 2, 3])
-  .concat(ns([4, 5, 6]), ns([7, 8, 9]))
-  .take(7)
-  .foreach((x) => console.log(x));
+function delay(n: number) {
+  return new Promise((r) => setTimeout(r, n));
+}
+
+ans
+  .race(
+    ans(async function* () {
+      while (true) {
+        await delay(Math.random() * 10);
+        console.log(1);
+        yield Math.random();
+      }
+    }),
+    ans(async function* () {
+      while (true) {
+        await delay(Math.random() * 20);
+        console.log(2);
+        yield Math.random();
+      }
+    }),
+    ans(async function* () {
+      while (true) {
+        await delay(Math.random() * 30);
+        console.log(3);
+        yield Math.random();
+      }
+    })
+  )
+  .take(30)
+  .foreach(async (x) => (await delay(100), console.log(x)));
